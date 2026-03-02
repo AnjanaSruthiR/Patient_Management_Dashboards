f501622ffb2417d34756b2a3dfba29d6f0bd82b5
AnjanaSruthiR <ranga.anj@northeastern.edu>
2026-03-01 22:48:35 -0500

    gh-pages

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,7 @@ import react from '@vitejs/plugin-react'
 import tailwindcss from '@tailwindcss/vite'
 
 export default defineConfig({
+  base: "/Patient_Management_Dashboards/",
   plugins: [
     react(),
     tailwindcss(),
